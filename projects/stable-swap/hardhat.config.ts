77da646be06053875691ca05da00f8bfcd2ae6fe
MUROKO <muroko@users.noreply.github.com>
2025-09-27 23:33:53 -0400

    update_2-1: hardhat.config-ts

--- projects/stable-swap/hardhat.config.ts
+++ projects/stable-swap/hardhat.config.ts
@@ -2,6 +2,7 @@ import type { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-web3";
 import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-etherscan";
 import "hardhat-abi-exporter";
 import "hardhat-contract-sizer";
 import "solidity-coverage";
@@ -25,12 +26,45 @@ const ticsMainnet: NetworkUserConfig = {
   accounts: [process.env.KEY_MAINNET!],
 };
 
-const config = {
+const { KEY_MAINNET } = process.env;
+
+const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    // testnet: bscTestnet,
-    // mainnet: bscMainnet,
+    hardhat: {
+      // Hardhat network configuration is for local development.
+      // It can be forked to mirror a live network for testing.
+    },
+//    mainnet: {
+//      url: "https://bsc-dataseed.binance.org/", // bscMainnet
+//	  	chainId: 56,
+//      accounts: [process.env.KEY_MAINNET!],
+//    },
+    mainnet: {
+      url: "https://rpc.qubetics.com/",		// ticsMainnet
+	  accounts: process.env.KEY_MAINNET
+        ? [process.env.KEY_MAINNET]
+        : [], // Safely handle the private key
+      chainId: 9030,
+	//  chainId: 9030,
+    //  accounts: [process.env.KEY_MAINNET!],
+    },
+  },
+  etherscan: {
+    // API key is not always required for custom explorers but good practice to include
+    apiKey: {
+      mainnet: 'YOUR_API_KEY', // Replace with your TicsScan API key if needed
+    },
+    customChains: [
+      {
+        network: 'mainnet',
+        chainId: 9030,
+        urls: {
+          apiURL: 'https://ticsscan.com/contract/verify', // TicsScan API URL
+          browserURL: 'https://ticsscan.com/', // TicsScan browser URL
+        },
+      },
+    ],
   },
   solidity: {
     compilers: [
