234611617c93a537b8c5d9981f9d99226a041afe
MUROKO <muroko@users.noreply.github.com>
2025-09-27 23:29:35 -0400

    add: bsc/config-ts

---
+++ projects/stable-swap/bsc/config.ts
@@ -0,0 +1,46 @@
+export default {
+  A: {
+    mainnet: 1000,
+    testnet: 1000,
+  },
+  Fee: {
+    mainnet: 4000000,
+    testnet: 4000000,
+  },
+  AdminFee: {
+    mainnet: 5000000000,
+    testnet: 5000000000,
+  },
+  LPFactory: {
+    mainnet: "0xc660C3a2Fab0003C4C67f9e95c9F992672B8aE2B",
+    testnet: "0x",
+  },
+  SwapTwoPoolDeployer: {
+    mainnet: "0x5f196977F3b4ac218b8d03b3D6228b30Da6ceD31",
+    testnet: "0x",
+  },
+  SwapThreePoolDeployer: {
+    mainnet: "0x022b8945882Fc9326cb151AD2DEEebA8518f1593",
+    testnet: "0x",
+  },
+  Factory: { //qubeStableSwapFactory 
+    mainnet: "0x2f0c4dF09Fac1aa410C94c4d97aF0186538057e3",
+    testnet: "0xe",
+  },
+  TwoPoolInfo: {
+    mainnet: "0x82B9fE12f9CB4D27459D822d84566E3ec597Fb28",
+    testnet: "0x",
+  },
+  ThreePoolInfo: {
+    mainnet: "0x33C8F2c69DD303F7B11EEC9FA787A886138ea860",
+    testnet: "0x",
+  },
+  Info: { //qubeStableSwapInfo
+    mainnet: "0x038ceb34fe309bcA14F92aaDe2E34a955b318f4C",
+    testnet: "0x",
+  },
+  WBNB: {
+    mainnet: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    testnet: "0x931Bf638fC27499506a4C1446e5f905b0EC73C81",
+  },
+};
