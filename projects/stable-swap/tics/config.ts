d3dbfab2b0797a24438d727746f5bb84f72bdc70
MUROKO <muroko@users.noreply.github.com>
2025-09-27 23:36:34 -0400

    add: tics/config-ts

---
+++ projects/stable-swap/tics/config.ts
@@ -0,0 +1,47 @@
+//Config - TICS
+export default {
+  A: {
+    mainnet: 1000,
+    testnet: 1000,
+  },
+  Fee: {
+    mainnet: 4000000,
+    testnet: 4000000,
+  },
+  AdminFee: {
+    mainnet: 5000000000,
+    testnet: 5000000000,
+  },
+  LPFactory: {
+    mainnet: "0xB6892b7bdFEED20629cB3A0F9940EE54F24Acf3E", //official
+    testnet: "0x",
+  },
+  SwapTwoPoolDeployer: {
+    mainnet: "0x5c28AF043d1Fa488A9D57836EBA989D60299083B", //official
+    testnet: "0x",
+  },
+  SwapThreePoolDeployer: {
+    mainnet: "0x71b07A3EC1402d614b7fC6de90719624b076f51F", //official
+    testnet: "0x",
+  },
+  Factory: { //qubeStableSwapFactory
+    mainnet: "0x155a543dB75999B4a4e831656941aD653537596b", //official
+    testnet: "0x",
+  },
+  TwoPoolInfo: {
+    mainnet: "0x094A2FeA494036136fBbD9De2475Fa828a056504", //official
+    testnet: "0x",
+  },
+  ThreePoolInfo: {
+    mainnet: "0x6364e2F2f544c2CD1Ea72b5c84B85456f7992AFD", //official
+    testnet: "0x",
+  },
+  Info: { //qubeStableSwapInfo
+    mainnet: "0x4f6ba916ffb6223678F31f9b75EdF569348A4815", //official
+    testnet: "0x",
+  },
+  WBNB: { // WTICS
+    mainnet: "0x9009A3171c92A733943118958312013989660943",
+    testnet: "0x",
+  },
+};
