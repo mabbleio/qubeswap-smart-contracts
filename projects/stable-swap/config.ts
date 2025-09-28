ecd2b3b552f2d4b517a6a38a8aa9282836696b47
MUROKO <muroko@users.noreply.github.com>
2025-09-27 23:30:16 -0400

    update: stable-swap/config-ts

--- projects/stable-swap/config.ts
+++ projects/stable-swap/config.ts
@@ -1,3 +1,4 @@
+//Config - TICS
 export default {
   A: {
     mainnet: 1000,
@@ -12,42 +13,35 @@ export default {
     testnet: 5000000000,
   },
   LPFactory: {
-    mainnet: "0x1179ADfa22dD0e5050C1c00C9f8543A77F75A2c0",
-    testnet: "0x17e7fEBaD84801b954e0076F8A8F1F66f4cD7Ad6",
-    goerli: "0xb224D46DD1e418588C8266F005E4bf9ff5f6b55c",
+    mainnet: "0xB6892b7bdFEED20629cB3A0F9940EE54F24Acf3E", //official
+    testnet: "0x",
   },
   SwapTwoPoolDeployer: {
-    mainnet: "0xdDFCDAacC836dd5A1AE2D375fFb153cE59DD09ff",
-    testnet: "0xb3E52e0956C6a1e9d2932AdE6B4AFf8748FEa4Da",
-    goerli: "0x2E564618dC39D2be3115dcF68b75e42bD8432087",
+    mainnet: "0x5c28AF043d1Fa488A9D57836EBA989D60299083B", //official
+    testnet: "0x",
   },
   SwapThreePoolDeployer: {
-    mainnet: "0x6AF7a605953C0B462EE9540217F5bD80878c6b2E",
-    testnet: "0x21AA173E1297cEb72b482297D9f7eE5a450b7606",
-    goerli: "0x534F89508FAc4b486c090e407bE204D6AC0462C0",
+    mainnet: "0x71b07A3EC1402d614b7fC6de90719624b076f51F", //official
+    testnet: "0x",
   },
-  Factory: {
-    mainnet: "0x25a55f9f2279A54951133D503490342b50E5cd15",
-    testnet: "0xe6A00f8b819244e8Ab9Ea930e46449C2F20B6609",
-    goerli: "0xB4AAc2AD66ab56CFFf03aD05f1D90ba889e5Aed7",
+  Factory: { //qubeStableSwapFactory
+    mainnet: "0x155a543dB75999B4a4e831656941aD653537596b", //official
+    testnet: "0x",
   },
   TwoPoolInfo: {
-    mainnet: "0x150c8AbEB487137acCC541925408e73b92F39A50",
-    testnet: "0x0A548d59D04096Bc01206D58C3D63c478e1e06dB",
-    goerli: "0x13e9f6c87b1BF0D1d0e1D94c1DF9c51939CA591C",
+    mainnet: "0x094A2FeA494036136fBbD9De2475Fa828a056504", //official
+    testnet: "0x",
   },
   ThreePoolInfo: {
-    mainnet: "0x5950eF6bA8edd7Dd30E9bCd05061ADAb78C58f16",
-    testnet: "0x69d565bDf0E7f0224dFb6f7257108090387c5Fe7",
-    goerli: "0x5a09ED72Cbc6c4d024f7E3eAbE487b1A8413cFEf",
+    mainnet: "0x6364e2F2f544c2CD1Ea72b5c84B85456f7992AFD", //official
+    testnet: "0x",
   },
-  Info: {
-    mainnet: "0x5A075D13469201cAd4Ac0c77aEFF31f2217A9448",
-    testnet: "0x734098fCB6ae5Cb0e249187f3657Cff05CEEf4F0",
-    goerli: "0x0a4922aD4400c920144adec825B8d4D814C48303",
+  Info: { //qubeStableSwapInfo
+    mainnet: "0x4f6ba916ffb6223678F31f9b75EdF569348A4815", //official
+    testnet: "0x",
   },
-  WBNB: {
-    mainnet: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-    testnet: "0x931Bf638fC27499506a4C1446e5f905b0EC73C81",
+  WBNB: { // WTICS
+    mainnet: "0x9009A3171c92A733943118958312013989660943",
+    testnet: "0x",
   },
 };
